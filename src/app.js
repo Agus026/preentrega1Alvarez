ad03d8db11e4f2b6fdd4c5d9aec956f4c7377a65
agusalvarez <agystinalvarez@gmail.com>
2024-08-06 23:26:40 -0300

    2preentregafinalAlvarez

--- src/app.js
+++ src/app.js
@@ -1,18 +1,51 @@
-const express = require ("express");
+
+const express = require("express");
 const app = express();
 const PUERTO = 8080;
-const productRouter = require ("./routes/products.router");
-app.use(productRouter)
-const cartRouter = require ("./routes/cart.router");
-app.use(cartRouter)
+const productRouter = require("./routes/products.router");
+app.use(productRouter);
+const cartRouter = require("./routes/cart.router");
+app.use(cartRouter);
+const viewsRouter = require("./routes/views.router");
+
+const socket = require("socket.io")
+
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+app.set("views", "./src/views");
 
 
+//Rutas
+app.use(express.json());
+app.use(express.static("./src/public"));
+app.use("/api/products", productRouter)
+app.use("/api/carts", cartRouter)
+app.use("/", viewsRouter)
+
+const httpServer = app.listen(PUERTO, () => {
+    console.log(`Escuchando en el puerto https:localhost:${8080}`)
+});
+
+const {ProductManager} = require("./controllers/product-manager")
+const manager = new ProductManager("./src/data/products.json")
 
 
+const io = socket(httpServer);
 
+io.on("connection", async (socket) => {
+    console.log("Un cliente se conecto");
 
+    socket.on("mensaje", (data) => {
+        console.log(data);
+    })
 
+    socket.emit("productos", await manager.getProducts());
 
-app.listen(PUERTO, () => {
-    console.log("Escuchando puerto 8080")
+    socket.on("eliminarProducto", async (id) =>{
+        await manager.eliminarProducto(id); 
+        io.socket.emit("productos", await manager.getProducts());
+    })
 })
+
